fe6004dd0068f7a1694b58730c846e8366e28d6b
leeminho <tommymh97@naver.com>
2024-04-24 20:03:54 +0900

    baseAlgo

--- src/main/java/baseAlgo/bj10952.java
+++ src/main/java/baseAlgo/bj10952.java
@@ -1,10 +1,26 @@
 package baseAlgo;
 
+import java.io.*;
+import java.util.*;
 public class bj10952 {
-    public static void main(String[] args){
-        int x = 2, y  = 10, z = 0;
-        z = x++*2+--y-5+x*(y%2);
-        System.out.println(z);
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        StringBuilder sb = new StringBuilder();
+
+        while (true){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int sum = a + b;
+            if (sum == 0) break;
+            sb.append(sum).append("\n");
+        }
+        System.out.print(sb);
+        br.close();
+
     }
 
 }
--- src/main/java/baseAlgo/bj10953.java
+++ src/main/java/baseAlgo/bj10953.java
@@ -1,8 +1,27 @@
 package baseAlgo;
 
+import java.io.*;
+import java.util.*;
 public class bj10953 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        StringBuilder sb = new StringBuilder();
+        int i = Integer.parseInt(br.readLine());
+
+        while (true) {
+            for(int j = 0; j < i; j++ ) {
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                int sum = a + b;
+                sb.append(sum).append("\n");
+            }
+        }
+        System.out.print(sb);
+        br.close();
 
     }
 }
