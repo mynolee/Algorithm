d35552c80bf0a7f80af876596cb8f0a78621dcd0
leeminho <tommymh97@naver.com>
2024-05-14 23:59:55 +0900

    baseAlgo

---
+++ src/main/java/baseDP/bj1463.java
@@ -0,0 +1,40 @@
+package baseDP;
+
+import java.util.*;
+import java.io.*;
+
+public class bj1463 {
+    public static void main(String[] args) throws IOException{
+
+        /*
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int num = Integer.parseInt(br.readLine());
+        int k = 0;
+
+        while(num != 1) {
+            k++;
+            if ((num % 3) == 0) {
+                num = num / 3;
+                if (num == 1) break;
+            } else if ((num % 2) == 0) {
+                num = num / 2;
+                if (num == 1) break;
+            } else num = num - 1;
+        }
+
+        System.out.println(k);
+        */
+
+        Integer[] dp;
+        static int recur(int N){
+
+            if(dp[N] == null) {
+                if()
+            }
+        }
+
+
+    }
+}
